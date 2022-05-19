762090255160c1b4597aba4f932ce1423bf3dc3a
3Xpl0it3r <shouc.wang@hotmail.com>
2022-05-20 00:30:56 +0800

    dp fib

---
+++ src/dp/code_509_fib.rs
@@ -0,0 +1,20 @@
+struct Solution{}
+
+
+impl Solution {
+    pub fn fib(n: i32) -> i32 {
+        if n <= 1 {
+            return n;
+        }
+        let mut dp_table =vec![0;(n+1) as usize];
+        dp_table[0] = 0;
+        dp_table[1] = 1;
+        for _i in 2..=n{
+            dp_table[_i as usize] = dp_table[(_i-1)as usize] + dp_table[(_i-2) as usize];
+        }
+        dp_table[n as usize]
+    }
+
+
+
+}
